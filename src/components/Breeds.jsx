e90ae30d35bcdeda14d2f885fecd8020b187f94b
Jacky Chen <tuo17505@temple.edu>
2022-06-29 03:36:07 -0400

    mib

--- src/components/Breeds.jsx
+++ src/components/Breeds.jsx
@@ -14,7 +14,8 @@ const Breeds = () => {
             margin: '0 auto',
             maxWidth: '980px',
             display: 'flex',
-            gap: '8px'
+            gap: '8px',
+            paddingTop: '16px'
         },
         description: {
             color: '#bdc1c6',
@@ -23,7 +24,8 @@ const Breeds = () => {
             fontSize: '22px',
             color: '#bdc1c6',
             fontWeight: 'bold',
-
+            maxWidth: '980px',
+            margin: 'auto'
         },
         link: {
             textDecoration: 'none',
@@ -31,6 +33,9 @@ const Breeds = () => {
         },
         img: {
             maxWidth: '300px',
+        },
+        boldt: {
+            fontWeight: 'bolder'
         }
     }
 
@@ -41,31 +46,32 @@ const Breeds = () => {
     if (breeds[0] && breeds[0]?.id !== '') {
         return (
             <div style={style.breeds}>
-                <div style={style.container} className='container'>
-                    <div style={style.description}>
-                        <h3 style={style.title}>Breed</h3>
-
-                        <h4>{breeds[0].name}</h4>
-
-                        <p>{breeds[0].description}</p>
-                        <p>Life Span: {breeds[0].life_span}</p>
-                        <p>Temperament: {breeds[0].temperament}</p>
-                        <p>
-                            More Resources: &nbsp;
-                            <a style={style.link} href="https://en.wikipedia.org/wiki/Bengal_(cat)">Wikipedia</a>  &emsp;
-                            <a style={style.link} href="http://cfa.org/Breeds/BreedsAB/Bengal.aspx">CFA</a> &emsp;
-                            <a style={style.link} href="http://www.vetstreet.com/cats/bengal">Vet Street</a> &emsp;
-                            <a style={style.link} href="https://vcahospitals.com/know-your-pet/cat-breeds/bengal">VCA Hospitals</a>
-                        </p>
+                <div style={style.description}>
+                    <h3 style={style.title}>Breed</h3>
+                    <div style={style.container} className='container'>
+                        <div>
+                            <h4>{breeds[0].name}</h4>
+                            <p>{breeds[0].description}</p>
+                            <p><span style={style.boldt}>Life Span:</span> {breeds[0].life_span}</p>
+                            <p><span style={style.boldt}>Temperament</span>: {breeds[0].temperament}</p>
+                            <p>
+                                <span style={style.boldt}>Resources:</span> &nbsp;
+                                <a style={style.link} href="https://en.wikipedia.org/wiki/Bengal_(cat)">Wikipedia</a>  &emsp;
+                                <a style={style.link} href="http://cfa.org/Breeds/BreedsAB/Bengal.aspx">CFA</a> &emsp;
+                                <a style={style.link} href="http://www.vetstreet.com/cats/bengal">Vet Street</a> &emsp;
+                                <a style={style.link} href="https://vcahospitals.com/know-your-pet/cat-breeds/bengal">VCA Hospitals</a>
+                            </p>
+                        </div>
+                        <img
+                            style={style.img}
+                            src={`https://cdn2.thecatapi.com/images/${breeds[0].reference_image_id}.png`}
+                            alt={breeds[0].name}
+                            onError={(e) => {
+                                e.target.src = `https://cdn2.thecatapi.com/images/${breeds[0].reference_image_id}.jpg`
+                            }}
+                        />
                     </div>
-                    <img
-                        style={style.img}
-                        src={`https://cdn2.thecatapi.com/images/${breeds[0].reference_image_id}.png`}
-                        alt={breeds[0].name}
-                        onError={(e) => {
-                            e.target.src = `https://cdn2.thecatapi.com/images/${breeds[0].reference_image_id}.jpg`
-                        }}
-                    />
+
                 </div>
             </div>
         )
